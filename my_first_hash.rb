1e1d321d431dba37ede2171cd71331707fb9afc0
Yancy Zhang <yuanxi.zhang@gmail.com>
2018-06-06 16:50:27 +0000

    automatically backed up by learn

--- my_first_hash.rb
+++ my_first_hash.rb
@@ -8,10 +8,12 @@ end
 
 
 def shipping_manifest
-  # set a variable called `the_manifest`, equal to a hash
+  # set a variable called ``, equal to a hash
   # fill that hash with key/value pairs that describe the following information: 
   # We have 5 whale bone corsets, 2 porcelain vases and 3 oil paintings 
-
+  the_manifest = {
+    ""
+  }
 
 end
 
